53d74e1469c1544b5152650eb73478afb6d29a07
stalking1eclipse <tlekgetho44@gmail.com>
2023-07-08 10:26:44 +0200

    Modifications.

---
+++ Assets/Scripts/IsGearEquipped.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsGearEquipped : MonoBehaviour
+{
+    public bool hardhat = false, 
+                vest = false, 
+                goggles = false, 
+                gloves = false, 
+                boots = false, 
+                mask = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
