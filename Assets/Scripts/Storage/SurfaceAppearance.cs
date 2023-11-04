a761eda9102045cec96cd3c6d09ac0b5d0b28b4a
Thabang Lekgetho <tlekgetho44@gmail.com>
2023-11-04 11:45:04 +0200

    Continued to develop the storage functionality. Storage spaces are able to detect debris now. Need to make sure that the storage spaces won't allow for storage when debris is present.

--- Assets/Scripts/Storage/SurfaceAppearance.cs
+++ Assets/Scripts/Storage/SurfaceAppearance.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class SurfaceAppearance : MonoBehaviour
 {
+    bool isSurfaceClean  {get;set;}
+
+    [SerializeField]
+    Material cleanMaterial;
+    [SerializeField]
+    Material debrisMaterial;
+
+    Collider[] debris;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isSurfaceClean = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        SetMaterial();
         
+        debris = Physics.OverlapBox(transform.position, new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z));
+
+        foreach (Collider collider in debris)
+        {
+            if (collider != null)
+            {
+                if (collider.CompareTag("Debris"))
+                {
+                    Debug.Log("Debris exists");
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z));
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Debris"))
+            isSurfaceClean = false;
+        else
+            isSurfaceClean = true;
+    }
+
+    void SetMaterial()
+    {
+        if (isSurfaceClean)
+            transform.gameObject.GetComponent<Renderer>().material = cleanMaterial;
+        else
+            transform.gameObject.GetComponent<Renderer>().material = debrisMaterial;
     }
 }
