926c89e2ae244c4b16d3933b96fe508500a47947
Sipho Mancam <siphomancam2@gmail.com>
2024-01-20 12:21:48 +0200

    Adjusted grab interactions

--- Assets/XRGrabInteractableTwoAttach.cs
+++ Assets/XRGrabInteractableTwoAttach.cs
@@ -3,20 +3,41 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRGrabInteractableTwoAttach : XRGrabInteractable
 {
-    public Transform LeftAttchPointTransform;
-    public Transform RightAttchPointTransform;
+    [SerializeField]
+    private Transform LeftAttchPointTransform;
+    [SerializeField]
+    private Transform RightAttchPointTransform;
     // Start is called before the first frame update
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
+        //attachTransform.position = args.interactorObject.transform.position;
+        //attachTransform.rotation = args.interactorObject.transform.rotation;
+        base.OnSelectEntered(args);
         if (args.interactorObject.transform.CompareTag("Left Hand"))
         {
-            attachTransform = LeftAttchPointTransform;
+            if (LeftAttchPointTransform)
+            {
+                attachTransform = LeftAttchPointTransform;
+            }
+            //else
+            {
+        
+            }
+
         }
         else if (args.interactorObject.transform.CompareTag("Right Hand"))
         {
-            attachTransform = RightAttchPointTransform;
+            if (LeftAttchPointTransform)
+            {
+                attachTransform = RightAttchPointTransform;
+            }
+            //else
+            //{
+            //    attachTransform.position = args.interactorObject.transform.position;
+            //    attachTransform.rotation = args.interactorObject.transform.rotation;
+            //}
+            //attachTransform = RightAttchPointTransform;
         }
 
-        base.OnSelectEntered(args);
     }
 }
